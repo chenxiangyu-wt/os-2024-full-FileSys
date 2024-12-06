b96d5e405581d4c4b529c7f39fdc7d3c9016fbee
van-artist <shangliu385@gmail.com>
2024-12-06 15:50:09 +0800

    feat: 添加FileSys.hpp头文件，定义目录项和目录结构

---
+++ FileSys.hpp
@@ -0,0 +1,24 @@
+#ifndef FILESYS_HPP
+#define FILESYS_HPP
+#include <cstdint>
+
+constexpr uint16_t NAMELEN = 255;
+constexpr uint16_t ENTRYNUM = 255;
+constexpr uint8_t DENTRY_FILE = 0;
+constexpr uint8_t DENTRY_DIR = 1;
+constexpr uint8_t DENTRY_EMPTY = 2;
+
+struct DirectoryEntry
+{
+    char name[NAMELEN];        // 文件或目录名称
+    unsigned int inode_number; // i 节点编号
+    uint8_t type;              // 文件类型
+};
+
+struct Directory
+{
+    DirectoryEntry entries[ENTRYNUM]; // 目录项数组
+    unsigned int entry_count;         // 当前目录中的有效项数
+};
+
+#endif // FILESYS_HPP
