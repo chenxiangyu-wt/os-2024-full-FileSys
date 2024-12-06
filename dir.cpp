9a7816215d75eb32ddd9afe55d851856ac134a99
van-artist <shangliu385@gmail.com>
2024-12-06 20:01:38 +0800

    fix: 修复初始化重复填充问题

--- dir.cpp
+++ dir.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 #include "filesys.h"
 
 void _dir()
--- install.cpp
+++ install.cpp
@@ -4,49 +4,61 @@
 
 void install()
 {
+	/* 定义常量 */
+	const size_t DirectoryEntrySize = ENTRYNUM + 4;				  // 目录项的大小
+	const size_t EntriesPerBlock = BLOCKSIZ / DirectoryEntrySize; // 每个块的目录项数量
 
-	/* 1. read the filsys from the superblock*/ // xiao
+	/* 1. 从超级块读取文件系统信息 */
 	memcpy(&fileSystem, disk + BLOCKSIZ, sizeof(FileSystem));
 
-	/* 2. initialize the inode hash chain*/
+	/* 2. 初始化 i-node 哈希链表 */
 	for (unsigned int i = 0; i < NHINO; i++)
 	{
 		hinode[i].prev_inode = NULL;
 	}
 
-	/*3. initialize the sys_ofile*/
+	/* 3. 初始化系统打开文件表 */
 	for (unsigned int i = 0; i < SYSOPENFILE; i++)
 	{
 		sys_ofile[i].reference_count = 0;
 		sys_ofile[i].inode = NULL;
 	}
 
-	/*4. initialize the user*/
+	/* 4. 初始化用户表 */
 	for (unsigned int i = 0; i < USERNUM; i++)
 	{
 		user[i].user_id = 0;
 		user[i].group_id = 0;
 		for (unsigned int j = 0; j < NOFILE; j++)
+		{
 			user[i].open_files[j] = SYSOPENFILE + 1;
+		}
 	}
 
-	/*5. read the main directory to initialize the dir*/
+	/* 5. 从主目录读取目录信息，初始化当前目录 */
 	cur_path_inode = iget(1);
-	dir.entry_count = cur_path_inode->file_size / (ENTRYNUM + 4); // xiao 2-->4
+	dir.entry_count = cur_path_inode->file_size / DirectoryEntrySize;
 
+	/* 初始化目录项数组 */
 	for (unsigned int i = 0; i < ENTRYNAMELEN; i++)
 	{
 		strcpy(dir.entries[i].name, "             ");
 		dir.entries[i].inode_number = 0;
 	}
+
+	/* 遍历并加载当前目录的所有目录项 */
 	unsigned int i;
-	for (i = 0; i < dir.entry_count / (BLOCKSIZ / (ENTRYNUM + 4)); i++)
+	for (i = 0; i < dir.entry_count / EntriesPerBlock; i++)
 	{
-		memcpy(&dir.entries[(BLOCKSIZ / (ENTRYNUM + 4)) * i],
-			   disk + DATASTART + BLOCKSIZ * cur_path_inode->block_addresses[i], DINODESIZ);
+		memcpy(
+			&dir.entries[EntriesPerBlock * i],
+			disk + DATASTART + BLOCKSIZ * cur_path_inode->block_addresses[i],
+			BLOCKSIZ);
 	}
 
-	memcpy(&dir.entries[(BLOCKSIZ) / (ENTRYNUM + 4) * i],
-		   disk + DATASTART + BLOCKSIZ * cur_path_inode->block_addresses[i], DINODESIZ);
-	return;
+	/* 加载剩余的目录项 */
+	memcpy(
+		&dir.entries[EntriesPerBlock * i],
+		disk + DATASTART + BLOCKSIZ * cur_path_inode->block_addresses[i],
+		BLOCKSIZ);
 }
